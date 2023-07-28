a734659f687818d180bc7590b53e84e126cf4baa
0xLucca <0xlucca.dev@gmail.com>
2023-07-28 12:35:41 -0300

    Format

--- tests/utils/convertions.ts
+++ tests/utils/convertions.ts
@@ -1,5 +1,5 @@
 export function hex_to_bytes(hex: string): number[] {
-    const buffer = Buffer.from(hex.slice(2), "hex");
-    const numbers: number[] = Array.from(buffer);
-    return numbers;
+  const buffer = Buffer.from(hex.slice(2), "hex");
+  const numbers: number[] = Array.from(buffer);
+  return numbers;
 }
