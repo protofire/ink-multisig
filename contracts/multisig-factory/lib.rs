3ddd894be8675b038a931b831f55743ec6b3a156
Gabriel Camba Gonzalez <gabrielnicolasgonzalez@gmail.com>
2023-08-09 17:35:06 -0300

    enhaced factory's comments

--- contracts/multisig-factory/lib.rs
+++ contracts/multisig-factory/lib.rs
@@ -1,37 +1,57 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+//!
+//! # MultiSigFactory
+//!
+//! This file contains the implementation of the MultiSigFactory contract.
+//!
+//! ## Overview:
+//! The MultiSigFactory contract is used to deploy new MultiSig contracts to
+//! keep track of the deployed MultiSig contracts and creation parameters.
+//!
+
 #[ink::contract]
 mod multisig_factory {
 
+    // Import the necessary dependencies.
     use ink::codegen::EmitEvent;
     use ink::prelude::vec::Vec;
     use ink::ToAccountId;
     use multisig::MultiSigRef;
 
+    /// The type encapsulating the events emitted by this contract.
     type Event = <MultiSigFactory as ink::reflect::ContractEventBase>::Type;
 
+    /// NewMultisig event emitted when a new MultiSig contract is deployed.
     #[ink(event)]
     pub struct NewMultisig {
+        /// The address of the deployed MultiSig contract.
         #[ink(topic)]
         multisig_address: AccountId,
+        /// The threshold of the deployed MultiSig contract.
         threshold: u8,
+        /// The list of owners of the deployed MultiSig contract.
         owners_list: Vec<AccountId>,
     }
 
+    /// Error type for the MultiSigFactory contract.
     #[derive(scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
-        /// The contract instantiation failed.
+        /// The contract instantiation has failed.
         InstantiationFailed,
     }
 
+    /// The MultiSigFactory struct to store the codehash of the MultiSig
     #[ink(storage)]
     #[derive(Default)]
     pub struct MultiSigFactory {
+        /// The codehash of the MultiSig contract.
         multisig_codehash: Hash,
     }
 
     impl MultiSigFactory {
+        /// Constructor that stores the codehash of the MultiSig contract.
         #[ink(constructor)]
         pub fn new(codehash: Hash) -> Result<Self, Error> {
             Ok(Self {
@@ -39,6 +59,11 @@ mod multisig_factory {
             })
         }
 
+        /// Deploy a new MultiSig contract.
+        /// The threshold and owners_list are passed as parameters.
+        /// The salt is passed as a parameter.
+        /// The multisig address is emitted as an event with the threshold and
+        /// owners_list.
         #[ink(message)]
         pub fn new_multisig(
             &mut self,
