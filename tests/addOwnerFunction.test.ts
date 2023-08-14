4faf936f080de826ccca2f66d9dc1b09f38b65d6
Gabriel Camba Gonzalez <gabrielnicolasgonzalez@gmail.com>
2023-08-14 20:47:40 -0300

    Added test to check should not add repeated owner

--- tests/addOwnerFunction.test.ts
+++ tests/addOwnerFunction.test.ts
@@ -98,8 +98,18 @@ describe("addOwnerFunction", () => {
     const rejections = (await multisig.query.getTxRejections(0)).value.ok;
     expect(rejections).to.equal(0);
 
+    //Listen for the event
+    let newTxExecutedEvent;
+    multisig.events.subscribeOnTransactionExecutedEvent((event) => {
+      newTxExecutedEvent = event;
+    });
+    
     // Approve the transaction by Bob
     await multisig.withSigner(bobKeyringPair).tx.approveTx(0);
+    
+    // Emit the success in the event result
+    expect(newTxExecutedEvent).to.exist;
+    expect(Object.keys(newTxExecutedEvent.result)).to.include("success")
 
     // Check the state after the execution of the transaction
     // Because the threshold is 2, the transaction is executed automatically and removed
@@ -113,6 +123,95 @@ describe("addOwnerFunction", () => {
     expect(newOwners).to.include(bobKeyringPair.address);
     expect(newOwners).to.include(charlieKeyringPair.address);
     expect(newOwners).to.include(daveKeyringPair.address);
+  });
+
+  it("Should not add a repeated owner", async () => {
+    // Index that allows to get the selector of a message by its label
+    const multisigMessageIndex = new MessageIndex(ContractAbi);
+
+    // Initial args
+    const init_threshold = 2;
+    const aliceKeyringPair = keyring.addFromUri("//Alice");
+    const bobKeyringPair = keyring.addFromUri("//Bob");
+    const charlieKeyringPair = keyring.addFromUri("//Charlie");
+
+    // Create a new contract
+    const constructors = new Constructors(api, aliceKeyringPair);
+
+    const { address } = await constructors.new(init_threshold, [
+      aliceKeyringPair.address,
+      bobKeyringPair.address,
+      charlieKeyringPair.address,
+    ]);
+    expect(address).to.exist;
+
+    // Bind the contract to the new address
+    const multisig = new Contract(address, aliceKeyringPair, api);
+
+    // Check the initial state
+    const threshold = (await multisig.query.getThreshold()).value.unwrap();
+    expect(threshold).to.equal(2);
+
+    const owners = (await multisig.query.getOwners()).value.unwrap();
+    expect(owners).to.have.lengthOf(3);
+
+    // Get the arguments for the proposeTx contract call
+
+    // Get the selector of the add_owner message
+    const selector = multisigMessageIndex.getMessageInfo("add_owner")?.selector.bytes;
+
+    // Create the argument for the add_owner message in the specified format
+    const arg = api.createType("AccountId", bobKeyringPair.address);
+    const arg_hex = arg.toHex();
+
+    const addOwnerTx: Transaction = {
+      address: address,
+      selector: selector!,
+      input: hex_to_bytes(arg_hex),
+      transferredValue: 0,
+      gasLimit: 100000000000,
+      allowReentry: true,
+    };
+
+    // Propose the transaction on chain
+    await multisig.tx.proposeTx(addOwnerTx);
+
+    // Check the state after the proposeTx call
+    let tx = await multisig.query.getTx(0);
+    expect(tx).to.exist;
+
+    // The proposed transaction has 1 approval and 0 rejections
+    const approvals = (await multisig.query.getTxApprovals(0)).value.ok;
+    expect(approvals).to.equal(1);
+
+    const rejections = (await multisig.query.getTxRejections(0)).value.ok;
+    expect(rejections).to.equal(0);
+
+    //Listen for the event
+    let newTxExecutedEvent;
+    multisig.events.subscribeOnTransactionExecutedEvent((event) => {
+      newTxExecutedEvent = event;
+    });
+
+    // Approve the transaction by Bob
+    await multisig.withSigner(bobKeyringPair).tx.approveTx(0);
+
+    // Emit the error in the event
+    expect(newTxExecutedEvent).to.exist;
+    expect(Object.keys(newTxExecutedEvent.result)).to.include("failed")
+    expect(Object.keys(newTxExecutedEvent.result.failed)).to.include("envExecutionFailed")
+
+    // Check the state after the execution of the transaction
+    // Because the threshold is 2, the transaction is executed automatically and removed
+    const tx_0 = (await multisig.query.getTx(0)).value.ok;
+    expect(tx_0).to.not.exist;
+
+    // Dave is added as a new owner
+    const newOwners = (await multisig.query.getOwners()).value.unwrap();
+    expect(newOwners).to.have.lengthOf(3);
+    expect(newOwners).to.include(aliceKeyringPair.address);
+    expect(newOwners).to.include(bobKeyringPair.address);
+    expect(newOwners).to.include(charlieKeyringPair.address);
   });
 
   it("Should not add a new owner when rejections make the aproval imposible to met", async () => {
