564669b1062e27674cf159c18aa4f82f621c0411
Henry Palacios <henrydpalacios@gmail.com>
2023-08-21 22:55:08 -0300

    Adding Github action

--- xsigners_sdk/src/MultisigFactory.ts
+++ xsigners_sdk/src/MultisigFactory.ts
@@ -1,24 +1,25 @@
 import { ApiPromise } from '@polkadot/api';
-import { ContractPromise } from '@polkadot/api-contract';
-import { ContractAbi } from '@typed_contracts/multisig-factory/contract-info/multisig_factory';
-import {ALL_CHAINS, isValidChain, CHAIN_CONTRACTS_ADDRESS} from "./constants"
+import type { KeyringPair } from '@polkadot/keyring/types';
+import { ContractAbi } from '../../typed_contracts/multisig-factory/contract-info/multisig_factory';
+import ContractMultisigFactory from '../../typed_contracts/multisig-factory/contracts/multisig_factory';
+import {ALL_CHAINS, isValidChain, CHAIN_CONTRACTS_ADDRESS} from "../../constants"
 
-export class MultisigFactory {
-  private nativeAPI: ApiPromise;
-  private chainId: ALL_CHAINS;
-
-  constructor(nativeAPI: ApiPromise, chainId: ALL_CHAINS) {
-    this.nativeAPI = nativeAPI;
-    this.chainId = chainId;
-  }
-
-  buildContractPromise(chainId?: ALL_CHAINS): ContractPromise {
-    const _chainId = chainId || this.chainId
-
-    if (!isValidChain(_chainId))
+export class MultisigFactorySdk {
+  public static factory(chainId: ALL_CHAINS, signer: KeyringPair, nativeAPI: ApiPromise) {
+    if (!isValidChain(chainId))
         throw new Error(`Unsupported chainId: ${chainId}`);
-      
-    const addressChain =  CHAIN_CONTRACTS_ADDRESS[_chainId]
-    return new ContractPromise(this.nativeAPI, ContractAbi, addressChain);
+
+    const addressChain =  CHAIN_CONTRACTS_ADDRESS[chainId]
+    
+    return new ContractMultisigFactory(addressChain, signer, nativeAPI)
+  }
+  
+  public static contractMetadata(chainId: ALL_CHAINS): { addressChain: string; ContractAbi: string; } {
+    if (!isValidChain(chainId))
+        throw new Error(`Unsupported chainId: ${chainId}`);
+    
+    const addressChain =  CHAIN_CONTRACTS_ADDRESS[chainId]
+    
+    return {addressChain, ContractAbi}
   }
 }
